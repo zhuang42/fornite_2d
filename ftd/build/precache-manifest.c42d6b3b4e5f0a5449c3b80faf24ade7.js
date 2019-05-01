478c1dc76b8ff74d122b3603b16df0fd6a9aaa78
ZichunZhuang <zichun.zhuang@mail.utoronto.ca>
2019-05-01 18:01:21 -0400

    builded react app

---
+++ ftd/build/precache-manifest.c42d6b3b4e5f0a5449c3b80faf24ade7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "659287570a8d7be067df",
+    "url": "/static/js/main.15e31835.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "36a0118ea8d3d3f5fcbb",
+    "url": "/static/js/2.909d4346.chunk.js"
+  },
+  {
+    "revision": "928e7b099ea9e4235e78b8172d7abd4e",
+    "url": "/index.html"
+  }
+];
